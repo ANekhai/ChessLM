9e0678fd640f0b1e00156709ef5d77fd8e5463e9
ANekhai <nekhai.anton@gmail.com>
2023-10-16 21:15:00 -0400

    init model files

---
+++ model.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+
+
+class TransformerDecoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self):
+        pass
+
+
+class MultiheadAttention(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self):
+        pass
